78c73cfa51431a768bc8aaf1e7d8f5ec8ed8f268
zalan96 <schzalan@gmail.com>
2026-03-18 08:26:45 +0100

    Comment test

--- SnapszerGame/game/Player.cs
+++ SnapszerGame/game/Player.cs
@@ -7,7 +7,7 @@ using System.Text;
 namespace SnapszerGame.game
 {
     public class Player : INotifyPropertyChanged
-    { //s
+    { //comment test
         public string Name { get; set; }
         public ObservableCollection<Card> Hand { get; set; }
         private int _score;

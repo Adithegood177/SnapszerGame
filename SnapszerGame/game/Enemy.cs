42a2752081c0781904a9e32cb1242773b3e0e87e
Ádám Norbert Németh <nemeth.adam.norbert@petrik.hu>
2026-04-03 19:40:24 +0200

    Kártyák látszódnak képek nem

--- SnapszerGame/game/Enemy.cs
+++ SnapszerGame/game/Enemy.cs
@@ -17,39 +17,29 @@ namespace SnapszerGame.game
             _rnd = new Random();
         }
 
-        // A gép hív
+
         public Card GepHiv(IEnumerable<Card> gepKezeben, Szin aduSzin)
         {
             var lapok = gepKezeben.ToList();
-
-            // TODO: 20/40 bemondás megnézése
-
-            // Sima Ász
             var asz = lapok.FirstOrDefault(l => l.ertek == Ertek.Asz && l.szin != aduSzin);
             if (asz != null) return asz;
 
-            // Sima Tízes
             var tizes = lapok.FirstOrDefault(l => l.ertek == Ertek.Tiz && l.szin != aduSzin);
             if (tizes != null) return tizes;
 
-            // Legkisebb nem adu
             var legkisebbNemAdu = lapok.Where(l => l.szin != aduSzin).OrderBy(l => l.pont).FirstOrDefault();
             if (legkisebbNemAdu != null) return legkisebbNemAdu;
-
-            // Ha már csak adu maradt, a legkisebbet dobjuk
             return lapok.OrderBy(l => l.pont).First();
         }
 
-        // A gép válaszol
         public Card GepValaszol(Card hivottLap, Szin aduSzin, bool pakliLezarva, IEnumerable<Card> gepKezeben)
         {
             var lapok = gepKezeben.ToList();
 
-            // Szabályosan lerakható lapok
+            
             var szabalyosLapok = lapok.Where(lap =>
                 _logic.SzabalyosKartyaRakas(lap, hivottLap, aduSzin, pakliLezarva, lapok)).ToList();
 
-            // Biztonsági csekkülés
             if (!szabalyosLapok.Any())
                 return lapok.First();
 
@@ -61,18 +51,16 @@ namespace SnapszerGame.game
             foreach (var lap in szabalyosLapok)
             {
                 if (lap.szin == hivottLap.szin && lap.pont > hivottLap.pont)
-                    valodiNyeroLapok.Add(lap); // Színre szín és nagyobb
+                    valodiNyeroLapok.Add(lap); 
                 else if (lap.szin == aduSzin && hivottLap.szin != aduSzin)
-                    valodiNyeroLapok.Add(lap); // Aduzás
+                    valodiNyeroLapok.Add(lap); 
             }
 
             if (valodiNyeroLapok.Any())
             {
-                // Legkisebb nyerő lappal viszünk
+
                 return valodiNyeroLapok.OrderBy(l => l.pont).First();
             }
-
-            // Ha esélytelen, eldobjuk a legkisebb lapot
             return szabalyosLapok.OrderBy(l => l.pont).First();
         }
     }
--- SnapszerGame/game/SnapszerLogic.cs
+++ SnapszerGame/game/SnapszerLogic.cs
@@ -7,57 +7,47 @@ namespace SnapszerGame.game
 {
     public class SnapszerLogic
     {
-        // Ütés nyertesének eldöntése
+        
         public Player GetWinnerOfTrick(Card hivottLap, Card valaszLap, Player hivoJatekos, Player valaszoloJatekos, Szin aduSzin)
         {
-            // Válaszoló aduzott (felülütötte a nem-adut)
             if (valaszLap.szin == aduSzin && hivottLap.szin != aduSzin)
             {
                 return valaszoloJatekos;
             }
-
-            // Színre színt, a nagyobb viszi
             if (hivottLap.szin == valaszLap.szin)
             {
                 return (hivottLap.pont > valaszLap.pont) ? hivoJatekos : valaszoloJatekos;
             }
 
-            // Különben a hívó viszi (kisebb dobás más színből)
             return hivoJatekos;
         }
 
-        // Szabványos lerakás validálása
         public bool SzabalyosKartyaRakas(Card lerakandoKartya, Card hivottLap, Szin aduSzin, bool pakliLezarva, IEnumerable<Card> jatekosKezeben)
         {
-            // Saját hívás, bármi lerakható
             if (hivottLap == null)
             {
                 return true;
             }
 
-            // Nyitott talonnál nincsenek kényszerek
             if (!pakliLezarva)
             {
                 return true;
             }
 
-            // --- LEZÁRT TALON: Színkényszer, ütéskényszer, adukényszer ---
 
             bool vanHivottSzin = jatekosKezeben.Any(lap => lap.szin == hivottLap.szin);
 
             if (vanHivottSzin)
             {
-                // Színkényszer
+                
                 if (lerakandoKartya.szin != hivottLap.szin) return false;
 
-                // Ütéskényszer
+               
                 bool vanNagyobbLapa = jatekosKezeben.Any(lap => lap.szin == hivottLap.szin && lap.pont > hivottLap.pont);
                 if (vanNagyobbLapa && lerakandoKartya.pont < hivottLap.pont) return false;
 
                 return true;
             }
-
-            // Adukényszer
             bool vanAduSzin = jatekosKezeben.Any(lap => lap.szin == aduSzin);
             if (vanAduSzin)
             {
@@ -65,11 +55,9 @@ namespace SnapszerGame.game
                 return true;
             }
 
-            // Nincs szín, nincs adu -> szabad dobás
             return true;
         }
 
-        // Kinyeri az elérhető bemondásokat (Király+Felső párok)
         public List<Szin> LehetsegesBemondasok(IEnumerable<Card> jatekosKezeben)
         {
             var bemondasok = new List<Szin>();
@@ -89,7 +77,6 @@ namespace SnapszerGame.game
             return bemondasok;
         }
 
-        // Bemondás pontértéke (sima=20, adu=40)
         public int BemondasErteke(Szin bemondottSzin, Szin aduSzin)
         {
             return (bemondottSzin == aduSzin) ? 40 : 20;

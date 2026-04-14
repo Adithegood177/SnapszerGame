2a5135c3b0dd32bb165646d335af310f2f0148a3
zalan96 <146957249+zalan96@users.noreply.github.com>
2026-04-14 18:43:47 +0200

    Updated Enemy.cs with casual Hungarian comments

---
+++ Enemy.cs
@@ -0,0 +1,19 @@
+// Ez itt a move() - ott csúszkálunk, mint az őrültek!
+public void move() {
+    // Húzzunk egy kis máját a pályán
+}
+
+// Ez a fight() - vetkőzés hősök között!
+public void fight() {
+    // Pindur-botmérkőzéseket szabadítsunk el
+}
+
+// Íme a levelUp() - szintugrások, mint a kiscicák!
+public void levelUp() {
+    // Húzzuk fel a szintet egy kicsit
+}
+
+// A loopban addig körözünk, míg le nem pörgeted az utolsó életed!
+for (int i = 0; i < lives; i++) {
+    // Zúzzuk agyba-főbe ezeket az ellenségeket!
+}

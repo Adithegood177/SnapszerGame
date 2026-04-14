367b5aa3cd88105a75e7cf5a54136b311d571f76
zalan96 <146957249+zalan96@users.noreply.github.com>
2026-04-14 18:38:24 +0200

    Add casual Hungarian comments to MainWindow.xaml.cs functions

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+// Ez itt a constructor, a window-on toljuk a dolgot.
+// Készít egy új játékot. Mivel ja, ez a funckó csinálja! 
+// Frissíti a UI-t a játék lépései alatt, annyira menő! 
+// Mindenkinek mutogatjuk, hogy mennyi pontja van, mert ez a lényeg.
+// Az gombokat vrendbe rakjuk, ahogy kell. 
+// Itt lerakja a játék végeredményét, sírni fogsz!
+// Játék módosítása, hogy ne halj meg az életben!
+// Ez az ábrázoló segít minket, hogy aztán rég ne érezzük magunkat "unatkozva".
+// Szóval ez a funckó állítja meg a játékot, ha már elegünk lett belőle!
